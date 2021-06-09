a02f08008a8d79f7481e7a351c942d1f29ae0d83
FedeM <federicomarilungo@gmail.com>
2021-06-09 13:47:01 -0300

    Add exploration feature

--- Attack.py
+++ Attack.py
@@ -1,6 +1,10 @@
 class Attack:
-  def __init__(self, origin, target, time, pcarga):
+  def __init__(self, origin, target, time, method, pcarga, gcarga, sondaespionaje = 1, crucero = 1):
     self.origin = origin
     self.target = target
     self.time = time
     self.pcarga = pcarga
+    self.gcarga = gcarga
+    self.sondaespionaje = sondaespionaje
+    self.crucero = crucero
+    self.method = method
--- ogame.py
+++ ogame.py
@@ -8,7 +8,6 @@ from Attack import Attack
 from datetime import datetime
 
 def wait_and_click(locator):
-    print("Wait and Click" + locator)
     WebDriverWait(driver, 2000).until(EC.presence_of_element_located((By.XPATH, locator)))
     iniciar_sesion_tab = driver.find_element_by_xpath(locator)
     iniciar_sesion_tab.click()
@@ -27,33 +26,42 @@ aceptar_cookies_button_locator = "/html/body/div[4]/div/div/span[2]/button[2]"
 
 driver = webdriver.Chrome()
 driver.get("https://lobby.ogame.gameforge.com/es_AR/?language=ar")
-# Save the window opener (current window, do not mistaken with tab... not the same)
 main_window = driver.current_window_handle
 sleep(2)
 driver.find_element_by_xpath(aceptar_cookies_button_locator).click()
 wait_and_click(iniciar_sesion_tab_locator)
-driver.find_element_by_name("email").send_keys("federicomarilungo@gmail.com")
+driver.find_element_by_name("email").send_keys("your_mail")
 sleep(1)
-driver.find_element_by_name("password").send_keys("Aj73kudw.33o")
+driver.find_element_by_name("password").send_keys("your_pass")
 sleep(1)
 driver.find_element_by_xpath("//*[@id='loginForm']/p/button[1]/span").click()
 sleep(3)
 driver.get("https://s135-ar.ogame.gameforge.com/game/index.php?page=ingame&component=overview&relogin=1")
 sleep(3)
 wait_and_click(jugado_por_ultima_vez_button_locator)
-driver.switch_to_window(main_window)
+driver.switch_to.window(main_window)
 sleep(2)
 
 
 #------------CONFIGURATION
 
-attacks = [Attack("33684507", "4:145:8", datetime(2021, 6, 2), "400"), # Uruguay -> Sandia [4:145:8]
-           Attack("33684545", "1:21:8", datetime(2021, 6, 2), "300"), # Madara -> Banana [1:21:8]
-           Attack("33684546", "2:125:8", datetime(2021, 6, 2), "300"), # Itachi -> Mango [2:125:8]
-           Attack("33684549", "2:136:11", datetime(2021, 6, 2), "300"), # Sakura -> Pomelo [2:136:11]
-           Attack("33684566", "1:21:5", datetime(2021, 6, 2), "300"), # Kiba -> Pera [1:21:5]
-           Attack("33684722", "2:148:9", datetime(2021, 6, 2), "400"), # Obito -> Mandarina [2:148:9]
-           Attack("33684797", "2:131:12", datetime(2021, 6, 2), "200")] # Deidara -> Limon [2:131:12]
+
+attacks = [
+           #Expediciones
+           Attack("33684507", "2:384:16", datetime(2021, 6, 2), "expedition", "9999", "9999"), # Uruguay
+           Attack("33684545", "2:385:16", datetime(2021, 6, 2), "expedition", "9999", "9999"), # Madara
+           Attack("33684546", "2:383:16", datetime(2021, 6, 2), "expedition", "9999", "9999"), # Itachi
+
+           #Ataques
+           Attack("33684549", "1:327:7", datetime(2021, 6, 2), "attack", "150", "0", "0", "0"), # Sakura -> Kr3 [1:327:7]
+           Attack("33684549", "2:136:11", datetime(2021, 6, 2), "attack", "150", "0", "0", "0"), # Sakura -> Pomelo [2:136:11]
+           Attack("33684566", "1:21:8", datetime(2021, 6, 2), "attack", "250", "0", "0", "0"), # Kiba -> Banana [1:21:8]
+           Attack("33684566", "1:327:9", datetime(2021, 6, 2), "attack", "250", "0", "0", "0"), # Kiba -> Kr2 [1:327:9]
+           Attack("33684722", "2:148:9", datetime(2021, 6, 2), "attack", "200", "0", "0", "0"), # Obito -> Mandarina [2:148:9]
+           Attack("33684722", "4:145:8", datetime(2021, 6, 2), "attack", "200", "0", "0", "0"), # Obito -> Sandia [4:145:8]
+           Attack("33684797", "2:125:8", datetime(2021, 6, 2), "attack", "150", "0", "0", "0"), # Deidara -> Mango [2:125:8]
+           Attack("33684797", "2:131:12", datetime(2021, 6, 2), "attack", "150", "0", "0", "0"), # Deidara -> Limon [2:131:12]
+           ]
 
 #------------BUCLE
 
@@ -71,23 +79,68 @@ while 1==1:
             try:
                 sleep(3)
                 cantidad_pcarga = driver.find_element_by_xpath(cantidad_pcarga_locator).get_attribute('innerText')
-                print("cantidad de pcarga : " + cantidad_pcarga)
             except:
                 sleep(3)
                 wait_and_click(jugado_por_ultima_vez_button_locator)
-                driver.switch_to_window(main_window)
+                driver.switch_to.window(main_window)
                 sleep(2)
                 driver.get("https://s135-ar.ogame.gameforge.com/game/index.php?page=ingame&component=fleetdispatch&cp=" + attack.origin)
                 sleep(3)
                 cantidad_pcarga = driver.find_element_by_xpath(cantidad_pcarga_locator).get_attribute('innerText')
-                print("cantidad de pcarga : " + cantidad_pcarga)
 
-            if int(cantidad_pcarga) >= int(attack.pcarga):
+            if attack.method == "attack":
+                if int(cantidad_pcarga) >= int(attack.pcarga):
+                    sleep(1)
+                    driver.find_element_by_xpath("//*[@id='civil']/li[1]/input").send_keys(attack.pcarga)
+                    sleep(1)
+                    driver.execute_script("window.scrollTo(0, 1080)")
 
+                    driver.find_element_by_xpath("//*[@id='continueToFleet2']/span").click()
+
+                    galaxy = attack.target.split(":")[0]
+                    system = attack.target.split(":")[1]
+                    position = attack.target.split(":")[2]
+
+                    sleep(1)
+                    driver.find_element_by_xpath(galaxy_input_locator).send_keys(galaxy)
+                    driver.find_element_by_xpath(system_input_locator).send_keys(system)
+                    driver.find_element_by_xpath(position_input_locator).send_keys(position)
+                    driver.find_element_by_xpath(position_input_locator).send_keys(Keys.ENTER)
+
+                    # Apretar en selección de misión, ataque
+                    sleep(2)
+                    wait_and_click("//*[@id='missionButton1']")
+
+                    sleep(2)
+                    returnTime = driver.find_element_by_xpath("//*[@id='returnTime']").get_attribute('innerText')
+
+                    fecha = returnTime.split(" ")[0]
+                    horario = returnTime.split(" ")[1]
+
+                    dia = int(fecha.split(".")[1])
+                    mes = int(fecha.split(".")[0])
+                    ano = int("20" + fecha.split(".")[2])
+
+                    hora = int(horario.split(":")[0])
+                    minuto = int(horario.split(":")[1])
+                    segundo = int(horario.split(":")[2])
+
+                    attack.time = datetime(ano, dia, mes, hora, minuto, segundo)
+
+                    # Apretar Atacar
+                    sleep(2)
+                    driver.find_element_by_xpath("//*[@id='deuterium']").send_keys(Keys.ENTER)
+                    print("ATAQUE EJECUTADO, regreso planificado: " + returnTime)
+
+            if attack.method == "expedition":
                 sleep(1)
-                driver.find_element_by_xpath("//*[@id='civil']/li[1]/span").click()
+                driver.find_element_by_xpath("//*[@id='military']/li[3]/input").send_keys(attack.crucero)
+                driver.find_element_by_xpath("//*[@id='civil']/li[1]/input").send_keys(attack.pcarga)
+                driver.find_element_by_xpath("//*[@id='civil']/li[2]/input").send_keys(attack.gcarga)
+                driver.find_element_by_xpath("//*[@id='civil']/li[5]/input").send_keys(attack.sondaespionaje)
+                driver.find_element_by_xpath("//*[@id='military']/li[10]/input").send_keys("1")
                 sleep(1)
-                driver.execute_script("window.scrollTo(0, 1080)") 
+                driver.execute_script("window.scrollTo(0, 1080)")
 
                 driver.find_element_by_xpath("//*[@id='continueToFleet2']/span").click()
 
@@ -101,13 +154,12 @@ while 1==1:
                 driver.find_element_by_xpath(position_input_locator).send_keys(position)
                 driver.find_element_by_xpath(position_input_locator).send_keys(Keys.ENTER)
 
-                #Apretar en selección de misión, ataque
+                # Apretar en selección de misión, ataque
                 sleep(2)
-                wait_and_click("//*[@id='missionButton1']")
+                wait_and_click("//*[@id='missionButton15']")
 
-                sleep(6)
+                sleep(2)
                 returnTime = driver.find_element_by_xpath("//*[@id='returnTime']").get_attribute('innerText')
-                print("Return Time : " + returnTime)
 
                 fecha = returnTime.split(" ")[0]
                 horario = returnTime.split(" ")[1]
@@ -121,12 +173,11 @@ while 1==1:
                 segundo = int(horario.split(":")[2])
 
                 attack.time = datetime(ano, dia, mes, hora, minuto, segundo)
-                
-                #Apretar Atacar
+
+                # Enviar Flota
                 sleep(2)
                 driver.find_element_by_xpath("//*[@id='deuterium']").send_keys(Keys.ENTER)
-                print("ATAQUEEEEEEEEEEEEEEEEEEEE EJECUTADO")
+                print("EXPEDICION EJECUTADA regreso planificado: " + returnTime)
 
-        # updetea la lista al proximo horario al que hay que atacar (horario de llegada +5 min)
     print("-------------------------------------------------------------")
     sleep(300)
